ed58a98b9570ba5923202a5045d182ff9a7f88fc
Chang Zhang <chang.zhang@rwth-aachen.de>
2026-09-09 20:04:35 +0200

    refactor: expose resumable post-training workspace

--- post_training/src/post_rl/training/__init__.py
+++ post_training/src/post_rl/training/__init__.py
@@ -2,6 +2,6 @@ from omegaconf import OmegaConf
 
 OmegaConf.register_new_resolver("eval", eval, replace=True)
 
-from .workspace import TrainACTWorkspace
+from .resumable_workspace import TrainACTWorkspace
 
 __all__ = ["TrainACTWorkspace"]
