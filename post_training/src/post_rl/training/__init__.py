b3f88454c5a18ae44a7fe59e95d6a73fab2d5504
Chang Zhang <chang.zhang@rwth-aachen.de>
2026-09-09 20:41:44 +0200

    fix: route training through final contract workspace

--- post_training/src/post_rl/training/__init__.py
+++ post_training/src/post_rl/training/__init__.py
@@ -2,6 +2,6 @@ from omegaconf import OmegaConf
 
 OmegaConf.register_new_resolver("eval", eval, replace=True)
 
-from .resumable_workspace import TrainACTWorkspace
+from .contract_workspace import TrainACTWorkspace
 
 __all__ = ["TrainACTWorkspace"]
